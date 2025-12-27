8c80827ed6ce25f110d30ca13999317068348760
premkumar-epic <premkumar.dev25@gmail.com>
2025-12-27 21:07:18 +0530

    Add root URL redirect to admin

--- backend/pregnancy/urls.py
+++ backend/pregnancy/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from rest_framework_simplejwt.views import TokenRefreshView
 from exercise.auth_serializers import CustomTokenObtainPairView
 from exercise.registration_views import register_user
 
 urlpatterns = [
+    # Redirect root to admin
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
+    
     path('admin/', admin.site.urls),
 
     # JWT Auth
